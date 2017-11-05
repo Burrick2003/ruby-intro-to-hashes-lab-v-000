a55f00f70055e97c653f4f3a36acec4017134745
Blakeley Bond <burrick2003@yahoo.com>
2017-11-05 04:41:35 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -55,7 +55,6 @@ def monopoly_with_fourth_tier
 									 },
 									 b_and_o_railroad: {
 										 mortgage_value: "$400"
-
 									 },
 									 shortline: {
 										 mortgage_value: "$800"
